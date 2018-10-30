70cb9572b67159429a06eb871c49e7f66d9e4b9b
kate <kimblek1@gmail.com>
2018-10-30 08:25:07 +0300

    Assignment To-Do-App: Complete

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ window.onload = function() {
 
 function onReady() {
 
+  let button = document.createElement('button');
+  document.body.appendChild(button);
+  button.textContent = "Delete Checked Item";
+  button.setAttribute('class', 'mdl-button mdl-js-button mdl-button--raised');
+  button.setAttribute('onclick', 'removeChecked()');
+
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
@@ -14,12 +20,23 @@ function onReady() {
     let title = newToDoText.value;
     let newLi = document.createElement('li');
     let checkbox = document.createElement('input');
+    checkbox.setAttribute('class', "mycheckbox");
 
     checkbox.type = "checkbox";
     newLi.textContent = title;
     newLi.appendChild(checkbox);
     toDoList.appendChild(newLi);
     newToDoText.value = '';
-
   });
+
 };
+
+function removeChecked() {
+  let mycheckboxes = document.getElementsByClassName('mycheckbox');
+
+  for (let i = 0; i < mycheckboxes.length; i++) {
+    if(mycheckboxes[i].checked == true) {
+      mycheckboxes[i].parentNode.remove();
+    }
+  }
+}
