12ad4e038409d6ddca03e55cc74e78726a64e999
kate <kimblek1@gmail.com>
2018-10-31 15:09:58 +0300

    checkpoint Dom Scripting Pt2: Complete

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
-window.onload = function() {
-  onReady();
-};
-
 function onReady() {
 
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', () => {
-    event.preventDefault();
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
 
-    let title = newToDoText.value;
-    let newLi = document.createElement('li');
-    let checkbox = document.createElement('input');
+    toDos.push({
+        title: newToDoText.value,
+        complete: false
+    });
 
-    checkbox.type = "checkbox";
-    newLi.textContent = title;
-    newLi.appendChild(checkbox);
-    toDoList.appendChild(newLi);
     newToDoText.value = '';
+    renderTheUI();
+  }
 
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
   });
+
+  renderTheUI();
+}
+
+window.onload = function() {
+  onReady();
 };
